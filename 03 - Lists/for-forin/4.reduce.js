5d2ef49b0f81ca1468984ca76d75c9280192bffd
Diego Pisani <diego.luidiblu@gmail.com>
2019-07-26 15:32:19 -0300

    Node

--- 03 - Lists/for-forin/4.reduce.js
+++ 03 - Lists/for-forin/4.reduce.js
@@ -22,7 +22,7 @@ async function main() {
         //     return anterior + proximo
         // }, 0)
         const minhaLista = [
-            ['Erick', 'Wendel'],
+            ['Diego', 'Pisani'],
             ['NodeBR', 'Nerdzão']
         ]
         const total = minhaLista.meuReduce((anterior, proximo) => {
